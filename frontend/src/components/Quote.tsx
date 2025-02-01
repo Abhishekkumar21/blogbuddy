e0df8c965ff8d31f3ad5f9e1ac040441e986bd2d
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2025-02-02 00:01:19 +0530

    feat: add quote component

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,11 @@
+export const Quote = () => {
+    return(
+        <div className="bg-blue-100 h-screen flex justify-center items-center">
+            <div className="flex flex-col gap-2 text-center">
+            <div className ="text-2xl font-bold">🌍 "Every great idea deserves a space. Start your blog today."</div>
+            <div className="text-sm italic font-semibold">💡 "Write. Inspire. Repeat."</div>
+            </div>           
+        </div>
+    )
+}
+
