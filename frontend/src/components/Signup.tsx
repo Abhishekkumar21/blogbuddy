089b7c9bfb85355376157296e0b9f89caa5e9153
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2025-02-03 22:48:03 +0530

    feat: implement signup functionality

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -2,6 +2,9 @@ import { useState } from "react"
 import { AuthHeader } from "./AuthHeader"
 import { LabelledInput } from "./LabelledInput"
 import { signupType } from "@abhie.npm/blogbuddy-common"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
+import { API_BASE_URL } from "../config/config"
 
 export const Signup = () => {
     const [postInputs, setPostInputs] = useState<signupType>({
@@ -9,6 +12,26 @@ export const Signup = () => {
         email: "",
         password: ""
     })
+
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+
+    const sendSignUpDeatailsToServer = async () =>{
+        setLoading(true);
+        const payload = postInputs;
+        try{
+            const response = await axios.post(API_BASE_URL+'/api/v1/user/signup', payload)
+            const token = response.data.jwtToken;
+            localStorage.setItem("ACCESS_TOKEN", token)
+            navigate("/blogs")                
+        
+        }catch(e){
+            //Alert the user here that signup request failed
+        } finally{
+            setLoading(false);
+        }
+      
+    }
   return (
           <div className="h-screen flex justify-center flex-col">
               <div className="flex justify-center">
@@ -27,7 +50,13 @@ export const Signup = () => {
                                        setPostInputs({...postInputs, password: e.target.value, })}}
                       />
                       </div>
-                      <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5  mt-4 w-full" >Sign up</button>
+                      <button onClick={sendSignUpDeatailsToServer} 
+                              type="button" 
+                              className="text-white bg-gray-800 disabled:cursor-progress hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-4 w-full"
+                              disabled = {loading}
+                        >
+                            {loading ? "Signing up..." : "Sign up"}
+                      </button>
                   </div>
               </div>
           </div>
